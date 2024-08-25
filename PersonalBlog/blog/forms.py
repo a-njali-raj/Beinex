3e7965465102be7fc75b22c41aa9be5eda7b8ef2
anjali <anjaliraj0308@gmail.com>
2024-08-25 22:25:55 +0530

    model for like

--- PersonalBlog/blog/forms.py
+++ PersonalBlog/blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Comment
+from .models import Comment,Like
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
--- PersonalBlog/blog/models.py
+++ PersonalBlog/blog/models.py
@@ -28,3 +28,11 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('post', 'user')
